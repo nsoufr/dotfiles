5b9bde7d2ba8a5ca9688abc4d5607963798a3c7c
Nando Sousa <nandosousafr@users.noreply.github.com>
2016-12-13 08:55:06 -0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,17 @@
 
 cd "$HOME";
 
+echo "Cloning nandosousafr/dotfiles..."
 git clone https://github.com/nandosousafr/dotfiles.git;
 
-function doIt() {
-  mv .vim vim-backup
-  ln -s dotfiles/dotvim .vim;
-  ln -s dotfiles/vimrc .vimrc;
+echo "Setting up...";
+mv .vim vim-backup
+ln -s dotfiles/dotvim .vim;
+ln -s dotfiles/vimrc .vimrc;
 
-	curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh;
-}
+echo "Fetching neobundle...";
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;	
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh;
+
+echo "Installing vim plugins";
+vim +NeoBundleInstall +qall
