3ee8979913d369cdda793d097db9d65ff2356f9e
Nando Sousa <nandosousafr@users.noreply.github.com>
2017-06-02 16:01:58 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,9 @@ git clone https://github.com/nandosousafr/dotfiles.git;
 echo "Setting up...";
 ln -s dotfiles/dotvim .vim;
 ln -s dotfiles/vimrc .vimrc;
+ln -s dotfiles/.gitignore_global .gitignore_global;
+
+git config --global core.excludesfile ~/.gitignore_global;
 
 echo "Fetching neobundle...";
 
