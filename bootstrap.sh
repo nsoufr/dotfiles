dbe7219ee030c7245ee320fb5dd8f7efd8e74bf2
Fernando Sousa <818744+nsoufr@users.noreply.github.com>
2024-02-22 19:29:49 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ cd "$HOME";
 sudo apt-get update
 sudo apt-get install -y vim zsh vim tmux xclip exuberant-ctags
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "Cloning nandosousafr/dotfiles..."
 git clone https://github.com/nandosousafr/dotfiles.git;
 
