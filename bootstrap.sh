343cab288e511c43111b897ccf536375b1addced
Nando Sousa <active.bell5726@fastmail.com>
2024-03-16 00:32:03 -0300

    initial nvim setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -24,12 +24,10 @@ echo 'Setting up Tmux';
 ln -s dotfiles/.tmux.conf .tmux.conf
 
 echo 'Setting up Neovim';
-mkdir -p $HOME/.config/nvim
-ln -s $HOME/dotfiles/init.vim .config/nvim/init.vim
+mkdir -p $HOME/.config && rm -rf $HOME/.config/nvim
+ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
 
 echo 'Installing vim plug';
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-
-nvim --headless +PlugInstall +qa
--- init.vim
+++
@@ -1,4 +0,0 @@
-call plug#begin(stdpath('config') . '/plugged')
-Plug 'vim-airline/vim-airline'
-Plug 'vim-airline/vim-airline-themes'
-call plug#end()
