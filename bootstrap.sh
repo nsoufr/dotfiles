499b30cb824fac4755ded8cba0e66a90b3d96f0c
nsoufr <818744+nsoufr@users.noreply.github.com>
2024-03-15 22:12:31 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,28 +2,36 @@
 
 cd "$HOME";
 
+echo 'Installing required packages'
+sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install -y vim zsh vim tmux xclip exuberant-ctags
+sudo apt-get install -y neovim zsh tmux xclip
 
+echo 'Installing oh my zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Cloning nandosousafr/dotfiles..."
-git clone https://github.com/nandosousafr/dotfiles.git;
+echo "Cloning nandosousafr/dotfiles...";
+git clone https://github.com/nandosousafr/dotfiles.git
 
-echo "Setting up...";
+echo "Setting up global git config";
+ln -s dotfiles/.gitignore_global .gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 
-ln -s dotfiles/dotvim .vim;
-ln -s dotfiles/vimrc .vimrc;
-ln -s dotfiles/.gitignore_global .gitignore_global;
+echo 'Setting up zsh config';
 rm ~/.zshrc && ln -s dotfiles/.zshrc .zshrc
 
-git config --global core.excludesfile ~/.gitignore_global;
+echo 'Setting up Tmux';
+ln -s dotfiles/.tmux.conf .tmux.conf
 
-ln -s dotfiles/.tmux.conf .tmux.conf;
+echo 'Setting up Neovim';
+mkdir -p $HOME/.config/nvim
+ln -s dotfiles/dotvim .config/nvim/init.vim
 
-echo "Fetching neobundle...";
+echo 'Installing vim plug';
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh;
 
-echo "Installing vim plugins";
-vim +NeoBundleInstall +qall &
+nvim --headless --cmd "call mkdir(stdpath('config'), 'p')" +q
+nvim --headless --cmd "'edit '.stdpath('config').'/init.vim'" +q
+nvim --headless +PlugInstall +q
