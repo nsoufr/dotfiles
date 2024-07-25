f83aa7e5eb57ea9085513cc1a9b9943c15745cec
Nando Sousa <light.lamp8513@fastmail.com>
2024-07-25 18:35:15 -0300

    simplify dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,28 @@
 
 cd "$HOME"
 
-echo 'Installing required packages'
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt-get update -y
-sudo apt-get install -y neovim zsh tmux xclip
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 
-echo 'Install neovim dependencies'
-sudo apt-get install -y ripgrep fzf
+if [ $machine == "Mac" ]; then
+       echo 'Installing brew'
+       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+       echo 'Installing required packages'
+       brew install zsh
+fi
+
+if [ $machine == "Linux" ]; then
+       echo 'Installing required packages'
+       sudo apt-get update -y
+       sudo apt-get install -y zsh
+fi
+
 
 echo 'Installing oh my zsh'
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -22,18 +37,3 @@ git config --global core.excludesfile ~/.gitignore_global
 
 echo 'Setting up zsh config'
 rm ~/.zshrc && ln -s dotfiles/.zshrc .zshrc
-
-echo 'Setting up Tmux'
-ln -s dotfiles/.tmux.conf .tmux.conf
-
-echo 'Setting up Neovim'
-mkdir -p $HOME/.config && rm -rf $HOME/.config/nvim
-ln -s $HOME/dotfiles/.config/nvim $HOME/.config/nvim
-
-echo 'Installing vim plug'
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-echo 'Fix for telescope-fzf-native error'
-# https://stackoverflow.com/questions/77435038/what-did-i-do-wrong-with-my-neovim-telescope-config
-cd ~/.local/share/nvim/lazy/telescope-fzf-native.nvim && make && cd -
